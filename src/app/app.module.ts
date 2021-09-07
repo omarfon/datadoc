d7ecb88d41187c4f8a4c80b293299ea69ca3b7cb
omar <omar.fonseca@aviva.pe>
2021-09-07 16:20:48 -0500

    inicializando repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { DataComponent } from './data/data.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AprobatedComponent } from './aprobated/aprobated.component';
+
+
+@NgModule({
+  declarations: [AppComponent,LoginComponent, DataComponent, AprobatedComponent],
+  entryComponents: [],
+  imports: [
+  BrowserModule, 
+  HttpClientModule,
+  FormsModule,
+  ReactiveFormsModule,
+  IonicModule.forRoot(), 
+  AppRoutingModule,
+  AngularFireModule.initializeApp(environment.firebaseConfig),
+  AngularFirestoreModule, // firestore
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ www/9605.45ac2b1cd9cf0c0972a0.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdataDoc=self.webpackChunkdataDoc||[]).push([[9605],{9605:(o,s,t)=>{"use strict";t.r(s),t.d(s,{ion_text:()=>a});var c=t(2322),r=t(7505),l=t(4843);const a=class{constructor(o){(0,c.r)(this,o)}render(){const o=(0,r.b)(this);return(0,c.h)(c.H,{class:(0,l.c)(this.color,{[o]:!0})},(0,c.h)("slot",null))}};a.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
